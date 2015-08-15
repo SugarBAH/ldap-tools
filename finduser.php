ae740d1868a85810d34bdd003457f9ad238ddccd
Brent Howe <bhowe@sugarcrm.com>
2015-08-15 15:24:09 -0700

    Mod 1

--- finduser.php
+++ finduser.php
@@ -3,16 +3,18 @@
 /* 
  This dumps all the data
  */
-
-include("config.php");
+echo "#1: $argv[1] #2: $argv[2] ";
+$search = "($argv[1]=$argv[2]*)"; 
+echo "search:  $search \n" ;
+include("ldap_config.php");
 
     $ds = ldap_connect($url) or die(ldap_error($ds));
     ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3) or die(ldap_error($ds));
     ldap_bind($ds, $admin, $pass) or die(ldap_error($ds));
 
     $output = "";
-/*    $sr = ldap_search($ds, $groupdn, '(cn=*)') or die(ldap_error($ds));  */ 
-   $sr = ldap_search($ds, $userdn, '(sn=*hart*)') or die(ldap_error($ds)); 
+#    $sr = ldap_search($ds, $groupdn, '(cn=*)') or die(ldap_error($ds));  
+    $sr = ldap_search($ds, $userdn, $search) or die(ldap_error($ds)); 
         $entries = array();
         echo "a";
 	$data = ldap_get_entries($ds, $sr) ;
